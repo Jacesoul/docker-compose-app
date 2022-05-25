f8c15b2e06019302dd132cfbc304b8be3e75457b
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-05-25 21:53:06 +0900

    Docker Compose 파일작성

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 // 숫자는 0부터 시작합니다.
 client.set("number", 0);
 app.get("/", (req, res) => {
-  client("number", (err, number) => {
+  client.get("number", (err, number) => {
     // 현재 숫자를 가져온 후에 1씩 올려줍니다.
     client.set("number", parseInt(number) + 1);
     res.send("숫자가 1씩 올라갑니다. 숫자 : " + number);
